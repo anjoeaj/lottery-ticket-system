158e5c1e4e7cabc3f5d1433664cbfb7eb8f87ee3
anjoeaj <anjoeaj@gmail.com>
2020-02-25 12:58:01 +0000

    added mongo db model for a ticket

---
+++ models/lottery-ticket.js
@@ -0,0 +1,25 @@
+/**
+ * Model for the Lottery Ticket
+ * 
+ * List of lines
+ * Status of the ticket
+ * 
+ */
+
+ let mongoose = require('mongoose');
+ let Schema = mongoose.Schema;
+ let line = require('./line');
+
+ let lotteryTicket = new Schema({
+     lines : [
+        {
+            //type: mongoose.Schema.Types.ObjectId,
+            //ref: "Line",
+            numbers : [Number],
+            outcome : Number
+        }
+     ],
+     status: Boolean
+ });
+
+ module.exports = mongoose.model("lottery_ticket", lotteryTicket);

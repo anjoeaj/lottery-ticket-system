8bbbdb6065a30a2e4c066892c65bf9a42a577b0a
anjoeaj <anjoeaj@gmail.com>
2020-02-26 06:06:02 +0000

    Refactored API endpoint names

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,16 @@ var router = express.Router();
 
 //Require controller modules
 // delete require.cache[require.resolve('../controllers/ticket')]
-var ticket_controller = require('../controllers/ticket');
+var ticketController = require('../controllers/ticket');
 
 router.get('/', (req, res, next) => {
     res.send("Welcome to Simple Lottery Ticket Web Service");
 });
 
-router.post('/ticket', ticket_controller.generate_ticket);
-router.get('/ticket', ticket_controller.ticket_list);
-router.get('/ticket/:id', ticket_controller.get_ticket);
-router.patch('/ticket/:id',ticket_controller.amend_ticket);
-router.patch('/status/:id', ticket_controller.ticket_status);
+router.post('/ticket', ticketController.generateTicket);
+router.get('/ticket', ticketController.ticketList);
+router.get('/ticket/:id', ticketController.getTicket);
+router.put('/ticket/:id',ticketController.amendTicket);
+router.put('/status/:id', ticketController.ticketStatus);
 
 module.exports = router;

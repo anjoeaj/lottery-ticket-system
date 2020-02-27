a81b70e3ed3fc9b82303f12dacd90d4652954d83
anjoeaj <anjoeaj@gmail.com>
2020-02-27 13:00:14 +0000

    additional tests and report

--- tests/line.test.js
+++ tests/line.test.js
@@ -8,19 +8,19 @@ describe('Compute Outcome', () => {
 });
 
 describe('Compute Outcome', () => {
-    it('should return outcome as 10', () => {
+    it('should return outcome as 5', () => {
         expect(line.computeOutcome([2, 2, 2])).toBe(5)
     })
 });
 
 describe('Compute Outcome', () => {
-    it('should return outcome as 10', () => {
+    it('should return outcome as 1', () => {
         expect(line.computeOutcome([2, 1, 0])).toBe(1)
     })
 });
 
 describe('Compute Outcome', () => {
-    it('should return outcome as 10', () => {
+    it('should return outcome as 0', () => {
         expect(line.computeOutcome([0, 1, 0])).toBe(0)
     })
 });

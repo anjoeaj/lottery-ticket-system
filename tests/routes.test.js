4a8af6aec0bba6c5a934858626210ba681934f09
anjoeaj <anjoeaj@gmail.com>
2020-02-27 01:17:10 +0000

    route testing

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,56 +1,56 @@
 
 
-// const request = require('supertest');
-// const app = require('../app');
+const request = require('supertest');
+const app = require('../app');
 
 // beforeAll(() => {
-//     return initializeCityDatabase();
+//     return initializeDatabase();
 // });
 
 // afterAll(() => {
-//     return clearCityDatabase();
+//     return clearDatabase();
 // });
 
 
-// describe('Post Endpoints', () => {
-//     it('should create a new post', async () => {
-//         const res = await request(app)
-//             .post('/ticket')
-//             .send({
-//                 "lines": [
-//                     [
-//                         0,
-//                         1,
-//                         1
-//                     ],
-//                     [
-//                         0,
-//                         0,
-//                         0
-//                     ]
-//                 ]
-//             })
-//         expect(res.statusCode).toEqual(201)
-//         //expect(res.body).toHaveProperty('post')
-//     })
-// });
+describe('Post Endpoints', () => {
+    it('should create a new post', async () => {
+        const res = await request(app)
+            .post('/ticket')
+            .send({
+                "lines": [
+                    [
+                        0,
+                        1,
+                        1
+                    ],
+                    [
+                        0,
+                        0,
+                        0
+                    ]
+                ]
+            })
+        expect(res.statusCode).toEqual(201)
+        //expect(res.body).toHaveProperty('post')
+    })
+});
 
-// describe('Get lottery ticket endpoints', () => {
-//     it('should get all lottery tickets', async () => {
-//         const res = await request(app)
-//             .get('/ticket')
-//             .send()
-//         expect(res.statusCode).toEqual(200)
-//         //expect(res.body).toHaveProperty('post')
-//     })
-// });
+describe('Get lottery ticket endpoints', () => {
+    it('should get all lottery tickets', async () => {
+        const res = await request(app)
+            .get('/ticket')
+            .send()
+        expect(res.statusCode).toEqual(200)
+        //expect(res.body).toHaveProperty('post')
+    })
+});
 
-// describe('Get status endpoint', () => {
-//     it('should get all lottery tickets', async () => {
-//         const res = await request(app)
-//             .get('/status/5e56f13b8c51b457d827b553')
-//             .send()
-//         expect(res.statusCode).toEqual(200)
-//         //expect(res.body).toHaveProperty('post')
-//     })
-// });
+describe('Get status endpoint', () => {
+    it('should get all lottery tickets', async () => {
+        const res = await request(app)
+            .get('/status/5e56f13b8c51b457d827b553')
+            .send()
+        expect(res.statusCode).toEqual(200)
+        //expect(res.body).toHaveProperty('post')
+    })
+});
